02dc4221d2efc679a658ade37de163309d1fc068
jabbydata <antonin.datas@gmail.com>
2025-07-19 11:31:39 +0900

    first customed chain

--- building_blocks.py
+++ building_blocks.py
@@ -7,25 +7,14 @@ import os
 import json
 
 # Workflow
-from langchain_openai import OpenAI
-from langchain_google_genai import GoogleGenerativeAI
-from langchain_huggingface import HuggingFacePipeline
-from transformers import pipeline
+from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_huggingface import ChatHuggingFace
+from langchain_huggingface.llms import HuggingFacePipeline
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from huggingface_hub import login
-
-
-def get_models():
-    try:
-        login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
-        pipe = pipeline("text-generation", model="mistralai/Mistral-7B-Instruct-v0.3")
-        llm = HuggingFacePipeline(pipeline=pipe)
-
-        prompt = "What is the capital of France?"
-        response = llm.invoke(prompt)
-        print(response)
-
-    except Exception as e:
-        print(e)
+from langchain_community.llms import FakeListLLM
 
 
 def set_api_keys(api_keys_path: str):
@@ -53,7 +42,95 @@ def set_api_keys(api_keys_path: str):
     print("API keys set !")
 
 
+def get_models():
+    try:
+        # Access a true model : Mistral-7B
+        login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
+        # model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+        model_id = "GPT2"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
+        llm = HuggingFacePipeline(
+            pipeline=pipe,
+            model_kwargs={
+                "temperature": 0.7,
+                "max_new_tokens": 100,
+            },
+        )
+
+        question_with_context_template = PromptTemplate(
+            "Context Information:{context}\n\n"
+            "Answer this question consisely:{question}"
+        )
+        prompt_text = question_with_context_template.format(
+            context="You are a weather forecast presentator",
+            question="What is the solution of this equation x**3 = -3 ?",
+        )
+
+        lowering_func = lambda x: x.lower()
+        chain_with_transfo = prompt_text | llm | lowering_func | StrOutputParser()
+
+        response = chain_with_transfo.invoke(prompt_text)
+        print(response)
+
+        # Access a false model
+        fake_llm = FakeListLLM(responses=["Hello"])
+        response = fake_llm.invoke(prompt_text)
+        print(response)
+
+    except Exception as e:
+        print(e)
+
+
+def chat_with_models():
+    try:
+        login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
+
+        # MIstral-7B
+        model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=20,
+            temperature=0.7,
+        )
+        llm = HuggingFacePipeline(pipeline=pipe)
+
+        chat = ChatHuggingFace(llm=llm, verbose=True)
+        messages = [
+            SystemMessage("You are a weather forecast presentator"),
+            HumanMessage("Tell me the weather forecast on 01/01/2015"),
+        ]
+        response = chat.invoke(messages)
+        print(response)
+
+        # GPT2
+        # model_id = "gpt2"
+        # tokenizer = AutoTokenizer.from_pretrained(model_id)
+        # model = AutoModelForCausalLM.from_pretrained(model_id)
+        # pipe = pipeline(
+        #     "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+        # )
+        # llm = HuggingFacePipeline(pipeline=pipe)
+
+        # chat = ChatHuggingFace(llm=llm, verbose=True)
+        # messages = [
+        #     SystemMessage("You are a weather forecast presentator"),
+        #     HumanMessage("Tell me the weather forecast on 01/01/2015"),
+        # ]
+        # response = chat.invoke(messages)
+        # print(response)
+
+    except Exception as e:
+        print(e)
+
+
 if __name__ == "__main__":
     api_keys_path = "langchain/config.json"
     set_api_keys(api_keys_path=api_keys_path)
-    get_models()
+    # get_models()
+    chat_with_models()
