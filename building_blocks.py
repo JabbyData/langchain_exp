be601577d393fdd1519604c043f247eae9e38c8f
JabbyData <antonin.datas@gmail.com>
2025-07-20 15:08:26 +0900

    complex chain example

--- building_blocks.py
+++ building_blocks.py
@@ -47,7 +47,7 @@ def get_models():
         # Access a true model : Mistral-7B
         login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
         # model_id = "mistralai/Mistral-7B-Instruct-v0.3"
-        model_id = "GPT2"
+        model_id = "gpt2"
         tokenizer = AutoTokenizer.from_pretrained(model_id)
         model = AutoModelForCausalLM.from_pretrained(model_id)
         pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
@@ -59,19 +59,9 @@ def get_models():
             },
         )
 
-        question_with_context_template = PromptTemplate(
-            "Context Information:{context}\n\n"
-            "Answer this question consisely:{question}"
-        )
-        prompt_text = question_with_context_template.format(
-            context="You are a weather forecast presentator",
-            question="What is the solution of this equation x**3 = -3 ?",
-        )
+        prompt_text = "What is the solution of x*3=-2 ?"
 
-        lowering_func = lambda x: x.lower()
-        chain_with_transfo = prompt_text | llm | lowering_func | StrOutputParser()
-
-        response = chain_with_transfo.invoke(prompt_text)
+        response = llm.invoke(prompt_text)
         print(response)
 
         # Access a false model
@@ -87,33 +77,16 @@ def chat_with_models():
     try:
         login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
 
-        # MIstral-7B
-        model_id = "mistralai/Mistral-7B-Instruct-v0.3"
-        tokenizer = AutoTokenizer.from_pretrained(model_id)
-        model = AutoModelForCausalLM.from_pretrained(model_id)
-        pipe = pipeline(
-            "text-generation",
-            model=model,
-            tokenizer=tokenizer,
-            max_new_tokens=20,
-            temperature=0.7,
-        )
-        llm = HuggingFacePipeline(pipeline=pipe)
-
-        chat = ChatHuggingFace(llm=llm, verbose=True)
-        messages = [
-            SystemMessage("You are a weather forecast presentator"),
-            HumanMessage("Tell me the weather forecast on 01/01/2015"),
-        ]
-        response = chat.invoke(messages)
-        print(response)
-
-        # GPT2
-        # model_id = "gpt2"
+        # # MIstral-7B
+        # model_id = "mistralai/Mistral-7B-Instruct-v0.3"
         # tokenizer = AutoTokenizer.from_pretrained(model_id)
         # model = AutoModelForCausalLM.from_pretrained(model_id)
         # pipe = pipeline(
-        #     "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+        #     "text-generation",
+        #     model=model,
+        #     tokenizer=tokenizer,
+        #     max_new_tokens=20,
+        #     temperature=0.7,
         # )
         # llm = HuggingFacePipeline(pipeline=pipe)
 
@@ -125,12 +98,96 @@ def chat_with_models():
         # response = chat.invoke(messages)
         # print(response)
 
+        # GPT2
+        model_id = "gpt2"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline(
+            "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=10
+        )
+        llm = HuggingFacePipeline(pipeline=pipe)
+
+        chat = ChatHuggingFace(llm=llm, verbose=True)
+        messages = [
+            SystemMessage("You are a weather forecast presentator"),
+            HumanMessage("Tell me the weather forecast on 01/01/2015"),
+        ]
+        response = chat.invoke(messages)
+        print(response)
+
+    except Exception as e:
+        print(e)
+
+
+def chain_exp():
+    try:
+        login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
+        model_id = "gpt2"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline(
+            "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100
+        )
+        llm = HuggingFacePipeline(pipeline=pipe)
+
+        prompt = PromptTemplate.from_template("Tell me a joke about {topic}")
+
+        out_parser = StrOutputParser()
+
+        format_func = lambda x: x.lower()
+
+        chain = prompt | format_func | llm | out_parser
+
+        response = chain.invoke({"topic": "programming"})
+
+        print(response)
+
+    except Exception as e:
+        print(e)
+
+
+def complex_chain_exp():
+    try:
+        login(os.environ["HUGGINGFACEHUB_API_TOKEN"])
+        model_id = "mistralai/Mistral-7B-Instruct-v0.3"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline(
+            "text-generation",
+            model=model,
+            tokenizer=tokenizer,
+            max_new_tokens=100,
+            temperature=0.7,
+        )
+        llm1 = HuggingFacePipeline(pipeline=pipe)
+
+        model_id = "gpt2"
+        tokenizer = AutoTokenizer.from_pretrained(model_id)
+        model = AutoModelForCausalLM.from_pretrained(model_id)
+        pipe = pipeline(
+            "text-generation", model=model, tokenizer=tokenizer, max_new_tokens=100
+        )
+        llm2 = HuggingFacePipeline(pipeline=pipe)
+        topic_prompt = PromptTemplate.from_template("Describe me the {topic}")
+        topic_chain = topic_prompt | llm1 | StrOutputParser()
+
+        analysis_prompt = PromptTemplate.from_template(
+            "Analysis the following information's mood: {info}"
+        )
+        analysis_chain = analysis_prompt | llm2 | StrOutputParser()
+
+        complex_chain = topic_chain | analysis_chain
+        complex_analysis = complex_chain.invoke({"topic": "bitcoin volatility"})
+
+        print("Analysis output : \n", complex_analysis)
+
     except Exception as e:
         print(e)
 
 
 if __name__ == "__main__":
-    api_keys_path = "langchain/config.json"
+    api_keys_path = "config.json"
     set_api_keys(api_keys_path=api_keys_path)
     # get_models()
-    chat_with_models()
+    # chat_with_models()
+    complex_chain_exp()
